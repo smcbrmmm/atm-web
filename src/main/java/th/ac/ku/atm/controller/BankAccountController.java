92b2bc8a72297418d1d0ee21da8f627d55cd6810
smcbrmmm <samut.c@ku.th>
2020-10-04 13:55:21 +0700

    add customer bankaccount feature

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -2,10 +2,7 @@ package th.ac.ku.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import th.ac.ku.atm.model.BankAccount;
 import th.ac.ku.atm.service.BankAccountService;
 
@@ -21,16 +18,59 @@ public class BankAccountController {
 
     @GetMapping
     public String getBankAccountPage(Model model) {
-        model.addAttribute("allBankAccounts", bankAccountService.getBankAccounts());
-        return "bankaccount";   // return login.html
+        model.addAttribute("allBankAccounts",bankAccountService.getBankAccountList());
+        return "bankaccount";
     }
 
     @PostMapping
-    public String openBankAccount(@ModelAttribute BankAccount bankAccount, Model model){
+    public String openBankAccount(@ModelAttribute BankAccount bankAccount, Model model) {
         bankAccountService.createBankAccount(bankAccount);
-        model.addAttribute("allBankAccounts", bankAccountService.getBankAccounts());
-//        System.out.println(bankAccountService.getBankAccounts().toString());
+        model.addAttribute("allBankAccounts", bankAccountService.getBankAccountList());
         return "redirect:bankaccount";
+    }
+
+    @GetMapping("/withdraw/{id}")
+    public String getWithdrawBankAccountPage(@PathVariable int id, Model model) {
+        BankAccount account = bankAccountService.getBankAccount(id);
+        model.addAttribute("bankAccount", account);
+        return "bankaccount-withdraw";
+    }
+
+    @GetMapping("/deposit/{id}")
+    public String getDepositBankAccountPage(@PathVariable int id, Model model) {
+        BankAccount account = bankAccountService.getBankAccount(id);
+        model.addAttribute("bankAccount", account);
+        return "bankaccount-deposit";
+    }
+
+//    @PostMapping("/edit/{id}")
+//    public String editAccount(@PathVariable int id, @ModelAttribute BankAccount bankAccount, Model model) {
+//        bankAccountService.editBankAccount(bankAccount);
+//        model.addAttribute("allBankAccounts", bankAccountService.getBankAccountList());
+//        return "redirect:/bankaccount";
+//    }
+
+    @PostMapping("/withdraw/{id}")
+    public String withdrawInAccount(@PathVariable int id, @ModelAttribute BankAccount bankAccount, Model model) {
+        bankAccountService.withdraw(id, bankAccount);
+        model.addAttribute("allBankAccounts", bankAccountService.getBankAccountList());
+        return "redirect:/bankaccount";
+    }
+
+    @PostMapping("/deposit/{id}")
+    public String depositInAccount(@PathVariable int id, @ModelAttribute BankAccount bankAccount, Model model) {
+        bankAccountService.deposit(id, bankAccount);
+        model.addAttribute("allBankAccounts", bankAccountService.getBankAccountList());
+        return "redirect:/bankaccount";
+    }
+
+
+    @PostMapping("/delete/{id}")
+    public String deleteAccount(@PathVariable int id, @ModelAttribute BankAccount bankAccount, Model model) {
+        bankAccountService.deleteBankAccount(bankAccount);
+        model.addAttribute("bankAccount", bankAccountService.getBankAccountList());
+        return "redirect:/bankaccount";
+
     }
 
 }
--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,26 +1,69 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import th.ac.ku.atm.model.BankAccount;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BankAccountService {
-    private List<BankAccount> bankAccounts;
+    private RestTemplate restTemplate;
 
-    @PostConstruct
-    public void postContruct(){
-        this.bankAccounts = new ArrayList<>();
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public void createBankAccount(BankAccount bankAccount){
-        bankAccounts.add(bankAccount);
+    public void createBankAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount";
+        restTemplate.postForObject(url, bankAccount, BankAccount.class);
     }
 
-    public List<BankAccount> getBankAccounts() {
-        return new ArrayList<>(this.bankAccounts);
+    public List<BankAccount> getBankAccountList() {
+        String url = "http://localhost:8091/api/bankaccount";
+        ResponseEntity<BankAccount[]> response = restTemplate.getForEntity(url, BankAccount[].class);
+        BankAccount[] accounts = response.getBody();
+        return Arrays.asList(accounts);
+    }
+
+    public List<BankAccount> getCustomerBankAccounts(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" + customerId;
+        ResponseEntity<BankAccount[]> response = restTemplate.getForEntity(url, BankAccount[].class);
+        BankAccount[] accounts = response.getBody();
+        return Arrays.asList(accounts);
+    }
+
+    public BankAccount getBankAccount(int id) {
+        String url = "http://localhost:8091/api/bankaccount/" + id;
+        ResponseEntity<BankAccount> response = restTemplate.getForEntity(url, BankAccount.class);
+        return response.getBody();
+    }
+
+    public void editBankAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/" + bankAccount.getId();
+        restTemplate.put(url, bankAccount);
+    }
+
+    public void withdraw(int id, BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/" + bankAccount.getId();
+        BankAccount b = getBankAccount(id);
+        b.setBalance(b.getBalance()-bankAccount.getBalance());
+        restTemplate.put(url, b);
+    }
+
+    public void deposit(int id, BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/" + bankAccount.getId();
+        BankAccount b = getBankAccount(id);
+        b.setBalance(b.getBalance()+bankAccount.getBalance());
+        restTemplate.put(url, b);
+    }
+
+    public void deleteBankAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/" + bankAccount.getId();
+        restTemplate.delete(url);
     }
 }
